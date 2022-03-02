a8068ea2b8211510b196d86b79a76fed4087167d
AhmedSlmn <adverts890@gmail.com>
2022-03-02 09:48:19 +0300

    Upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+    $(".icon").click(function () {
+
+        $(".links ul").toggleClass("show")
+    })
+
+   
+
+    // Smoothly Scroll To Elment
+    $(".links ul li a").click(function (e) {
+
+        e.preventDefault();
+
+        $(".links ul").toggleClass("show");
+
+        document.querySelector($(this).data("section")).scrollIntoView({
+
+            behavior: "smooth"
+
+        });
+
+    });
+
+    // Show The Arrow-up If Window scrollY Is >= 700
+    $(window).scroll(function () {
+
+        if (this.scrollY >= 700) {
+
+            $(".fa-arrow-up").fadeIn("slow")
+
+        } else {
+            $(".fa-arrow-up").fadeOut("slow")
+        }
+    })
+
+    // Click To Go Up
+    $(".fa-arrow-up").click(function () {
+
+        $(window).scrollTop(0)
+    })
+})
